b5b988e2f8c35f0e3cf327116b2c31e658e8809a
ugo-bertrand <ugo.bertrand@epitech.eu>
2023-05-29 16:02:22 +0200

    suppression des fichiers pour les utilisateurs

--- src/main/java/com/example/ecommerce/ECommerceApplication.java
+++ src/main/java/com/example/ecommerce/ECommerceApplication.java
@@ -1,13 +1,8 @@
 package com.example.ecommerce;
 
-import com.example.ecommerce.user.User;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 @SpringBootApplication
 @EnableMongoRepositories
